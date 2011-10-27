7abffaf3ad0d439157823b92ac567ef00caca60b
Gergely Harmat <gergely.harmat@gmail.com>
2011-10-28 01:46:28 +0200

    Finished layout and routes

---
+++ sample_app-1 m�solata/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'aff54c156e0627bb1750b10d425a6cc137c04b5264268da9e85b340edefb87fd74be1e818401290deb67d2c1c22dcc2a5f0765a5dae19c8ce2d7d8277b9ac044'
