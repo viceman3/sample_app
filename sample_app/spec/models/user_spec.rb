3ecbcbe71c5e2a309037c374caaccf6bbec5b30e
Gergely Harmat <gergely.harmat@gmail.com>
2011-10-28 09:31:17 +0200

    Finished first cut of the User model

--- sample_app/spec/models/user_spec.rb
+++ sample_app/spec/models/user_spec.rb
@@ -33,7 +33,7 @@ describe User do
       valid_email_user = User.new(@attr.merge(:email => address))
       valid_email_user.should be_valid
     end
-  end
+end
 
   it "should reject invalid email addresses" do
     addresses = %w[user@foo,com user_at_foo.org example.user@foo.]
@@ -41,7 +41,6 @@ describe User do
       invalid_email_user = User.new(@attr.merge(:email => address))
       invalid_email_user.should_not be_valid
     end
-  end
 end
 
   it "should reject duplicate email addresses" do
@@ -51,11 +50,12 @@ end
     user_with_duplicate_email.should_not be_valid
   end
 
+
   it "should reject email addresses identical up to case" do
     upcased_email = @attr[:email].upcase
     User.create!(@attr.merge(:email => upcased_email))
     user_with_duplicate_email = User.new(@attr)
     user_with_duplicate_email.should_not be_valid
   end
-end
+  end
 
